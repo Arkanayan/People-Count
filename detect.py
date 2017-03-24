949ca4ce866d5d16f6966075c6bc9837983db52a
Arka Nayan <itsarkanayan@gmail.com>
2017-03-24 13:52:07 +0530

    Support for command line argument

--- detect.py
+++ detect.py
@@ -4,21 +4,19 @@ import numpy
 import imutils
 import cv2
 import subprocess as sp
+import argparse
+import os
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--video", type=str, help="Path to the video file")
+args = vars(parser.parse_args())
 
-
-# VIDEO_URL = "https://manifest.googlevideo.com/api/manifest/hls_playlist/id/l5vUW5ZRHK0.0/itag/94/source/yt_live_broadcast/requiressl/yes/ratebypass/yes/live/1/cmbypass/yes/goi/160/sgoap/gir%3Dyes%3Bitag%3D140/sgovp/gir%3Dyes%3Bitag%3D135/hls_chunk_host/r2---sn-5jucgv5qc5oq-cagz.googlevideo.com/gcr/in/playlist_type/DVR/mm/32/mn/sn-5jucgv5qc5oq-cagz/ms/lv/mv/m/pl/19/dover/6/upn/WqYvM_XDWOM/beids/%5B9452306%5D/mt/1488196396/ip/106.51.66.100/ipbits/0/expire/1488218103/sparams/ip,ipbits,expire,id,itag,source,requiressl,ratebypass,live,cmbypass,goi,sgoap,sgovp,hls_chunk_host,gcr,playlist_type,mm,mn,ms,mv,pl/signature/252042428928E4725F652130CC6C7E97B35F0B87.339052FF9F19DC67DFCD137B184856DF60CCC64D/key/dg_yt0/playlist/index.m3u8"
-# VIDEO_URL = 'https://r2---sn-5jucgv5qc5oq-cagz.googlevideo.com/videoplayback/id/853532cc5e2ff4a0/itag/134/source/youtube/requiressl/yes/mm/31/initcwndbps/2950000/mn/sn-5jucgv5qc5oq-cagz/pcm2cms/yes/pl/19/ms/au/mv/m/ratebypass/yes/mime/video%2Fmp4/otfp/1/gir/yes/clen/11779214/lmt/1459875326930074/dur/156.990/signature/0E3EE9CCFB826CAF4FE4D403BAB00B4F4150E6FA.989C85682F9DB78BB15AFD5BDA17AD37CBDF31D1/key/dg_yt0/mt/1488534207/beids/%5B9465692%5D/upn/GTh4RMqGqlc/ip/106.51.66.100/ipbits/0/expire/1488555913/sparams/ip,ipbits,expire,id,itag,source,requiressl,mm,initcwndbps,mn,pcm2cms,pl,ms,mv,ratebypass,mime,otfp,gir,clen,lmt,dur/'
-
-# FFMPEG_BIN = '/usr/bin/ffmpeg'
-
-# pipe = sp.Popen([ FFMPEG_BIN, "-i", VIDEO_URL,
-
-#            "-an",   # disable audio
-#            "-f", "image2pipe",
-#            "-pix_fmt", "bgr24",
-#            "-vcodec", "rawvideo", "-"],
-#            stdin = sp.PIPE, stdout = sp.PIPE, bufsize=10**9)
+videoFile = None
+if args.get("video", None) is None or not os.path.isfile(args.get("video", None)):
+    print("Please check the path of the video file")
+    exit()
+else:
+    videoFile = args.get("video", None)
 
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
@@ -26,7 +24,7 @@ hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 fourcc = cv2.VideoWriter_fourcc(*'MP4V')
 out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (640,480))
 
-cap = cv2.VideoCapture('/home/arka/MantraLabs/Emotion/random_people_walk.webm')
+cap = cv2.VideoCapture(videoFile)
 
 index = 0
 frameRate = cap.get(cv2.CAP_PROP_FPS)
